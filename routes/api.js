51d0b7980750047d83f37eabceef4951a9d06cbf
M R V I K O <vikodk67@gmail.com>
2021-09-05 23:05:57 +0700

    883

--- routes/api.js
+++ routes/api.js
@@ -885,7 +885,7 @@ router.get('/id/epep', async (req, res, next) => {
 	var id = req.query.id;
             
 	if(!Apikey) return res.json(loghandler.notparam)
-	if(!id) return res,json({'Pesan_untuk_anda': 'Masukkin id nya ngen, dah buriq lapan bit, gada pintu, suara kaleng lgi awokwowkwowwok'})
+	if(!id) return res.json({'Pesan_untuk_anda': 'Masukkin id nya ngen, dah buriq lapan bit, gada pintu, suara kaleng lgi awokwowkwowwok'})
 	if(listkey.includes(Apikey)){
 
        fetch(encodeURI(`https://api.nichicodex.xyz/idFF/?id=${id}&type=json`))
