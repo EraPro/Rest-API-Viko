eee9b4aaa4350cf6a3777ac95c152ae15ab4d0c2
M R V I K O <vikodk67@gmail.com>
2021-10-07 13:57:01 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -155,7 +155,7 @@ loghandler = {
         status: false,
         creator: `${creator}`,
         code: 404,
-        result: 'APIKEY SALAH, Silahkan kunjungi viko-api.herokuapp.com terlebih dahulu'
+        result: 'APIKEY SALAH, https://viko-api.herokuapp.com/docs. pastikan anda mengunjungi situs nya untuk melihat apikeynya'
     },
     invalidlink: {
         status: false,
@@ -170,7 +170,7 @@ loghandler = {
     error: {
         status: false,
         creator: `${creator}`,
-        result: '500 internal server error viko-api.herokuapp.com'
+        result: '500 - internal server error, silahkan tunggu beberapa saat'
     }
 }
 
