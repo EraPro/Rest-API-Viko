d3a13cd374bde1ccc7e96a84318cf51422f245e5
M R V I K O <vikodk67@gmail.com>
2021-11-09 19:40:53 +0700

    Update ruang-admin.js

--- public/js/ruang-admin.js
+++ public/js/ruang-admin.js
@@ -150,7 +150,7 @@ var x = setInterval(function() {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)); 
     var seconds = Math.floor((distance % (1000 * 60)) / 1000); 
- 
+    document.getElementById("speed-test").innerHTML = res.speed_server
     // Output the result in an element with id="demo" 
     document.getElementById("vikoo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s" 
 }, 1000); 
