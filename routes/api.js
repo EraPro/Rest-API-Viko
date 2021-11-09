c57919f307e202de20bf9c37f042cf62520f54f9
M R V I K O <vikodk67@gmail.com>
2021-11-09 22:09:38 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -727,13 +727,13 @@ router.get('/pendidikan/brainly', async(req, res, next) => {
   if(!query) return res.json(loghandler.notquery)
   
   if(listkey.includes(apikey)){
-        brainly(query).then(res =>
-             res.json({
+	  .then(response => response.json())
+        brainly(query).then(res => res.json({
                  status : true,
                  creator : `${creator}`,
                  result : res
-             })
-        );
+		})
+        });
 } else {
 res.sendFile(__path + '/views/apikey-not-found.html');
 }
